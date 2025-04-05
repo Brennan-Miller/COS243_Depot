887712320d6a31d7ea3e5687498e4b52718d18f6
Brennan_Miller <bmanmiller250@gmail.com>
2025-04-04 21:25:53 -0400

    all of chapter 9

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -5,6 +5,15 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     @product = products(:one)
     @title = "The Great Book #{rand(1000)}"
   end
+  
+  test "can't delete product in cart" do
+    assert_difference("Product.count", 0) do
+      delete product_url(products(:two))
+    end
+    assert_redirected_to products_url
+  end
+
+
 
   test "should get index" do
     get products_url
